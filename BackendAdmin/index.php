75fc8a1b84e6a36976cc55e9b5ee4cbe5ff9abe3
Eddy <eryj2000@gmail.com>
2018-10-23 23:58:09 -0500

    Delete Proyect

--- BackendAdmin/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'Controller/controller.php';
-require_once 'Model/crud.php';
-require_once 'Model/enlace.php';
-             
-
- include "Views/templete.php";
--- justPay/check-out/index.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-require_once("Controller/controller.php");
-require_once "Model/crud.php";
-require_once ("Model/PaymentMethod/SafetyPay/Safetypay.php");
- $a = new controller();
- $a -> pagina();
-
